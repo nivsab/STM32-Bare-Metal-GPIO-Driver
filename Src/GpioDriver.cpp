2752158ebde54a716bce86e32c7038a60fd6c9c4
nivsab <niv.saban@gmail.com>
2026-02-24 15:58:36 +0200

    Update GpioDriver.cpp

--- Src/GpioDriver.cpp
+++ Src/GpioDriver.cpp
@@ -167,6 +167,74 @@ void write(bool state) {
         pGPIOx->ODR ^= (1 << pinConfig.GPIO_PinNumber);
     }
 
+void GPIO_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi)
+{
+// ISER (Interrupt Set-Enable Register), ICER (Interrupt Clear-Enable Register)
+	if(EnorDi == ENABLE)
+	{
+		if(IRQNumber <= 31)
+		{
+			//program ISER0 register
+			*NVIC_ISER0 |= ( 1 << IRQNumber );
+
+		}else if(IRQNumber > 31 && IRQNumber < 64 ) //32 to 63
+		{
+			//program ISER1 register
+			*NVIC_ISER1 |= ( 1 << (IRQNumber % 32) );
+		}
+		else if(IRQNumber >= 64 && IRQNumber < 96 )
+		{
+			//program ISER2 register //64 to 95
+			*NVIC_ISER2 |= ( 1 << (IRQNumber % 64) );
+		}
+	}else
+	{
+		if(IRQNumber <= 31)
+		{
+			//program ICER0 register
+			*NVIC_ICER0 |= ( 1 << IRQNumber );
+		}else if(IRQNumber > 31 && IRQNumber < 64 )
+		{
+			//program ICER1 register
+			*NVIC_ICER1 |= ( 1 << (IRQNumber % 32) );
+		}
+		else if(IRQNumber >= 64 && IRQNumber < 96 )
+		{
+			//program ICER2 register
+			*NVIC_ICER2 |= ( 1 << (IRQNumber % 64) );
+		}
+	}
+
+}
+
+
+void GPIO_IRQPriorityConfig(uint8_t IRQNumber,uint32_t IRQPriority)
+{
+	//1. first lets find out the ipr register
+	uint8_t iprx = IRQNumber / 4;
+	uint8_t iprx_section  = IRQNumber %4 ;
+
+	uint8_t shift_amount = ( 8 * iprx_section) + ( 8 - NO_PR_BITS_IMPLEMENTED) ;
+
+	*(  NVIC_PR_BASE_ADDR + iprx ) |=  ( IRQPriority << shift_amount );
+
+}
+
+void GPIO_IRQHandling()
+{
+/* * NOTE: For this code to execute, two flags must be '1' (Logical AND):
+ * 1. IMR (Interrupt Mask): Set manually by SOFTWARE during Init (The "Gate Keeper").
+ * 2. PR (Pending Bit): Set automatically by HARDWARE on edge detection (The "Event").
+ */
+if(EXTI->PR & ( 1 << this->pinConfig.GPIO_PinNumber)) 
+{
+    // Clear the pending bit (W1C) to acknowledge the interrupt
+    EXTI->PR |= ( 1 << this->pinConfig.GPIO_PinNumber);
+}
+
+}
+
+
 
 
 
